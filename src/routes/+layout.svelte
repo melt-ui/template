83a833bc83efe64574fb854f8606991cd50d7bf3
tglide <26071571+TGlide@users.noreply.github.com>
2023-07-14 17:20:01 +0100

    tailwind & accordion cmp

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.postcss';
+</script>
+
+<div class="container py-4">
+	<h1 class="text-4xl font-semibold">Melt UI Playground</h1>
+	<div class="py-4">
+		<slot />
+	</div>
+</div>
