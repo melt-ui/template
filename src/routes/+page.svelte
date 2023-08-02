3831fbd252bcf6e451ddb367807fb02392b2c937
tglide <26071571+TGlide@users.noreply.github.com>
2023-08-02 13:46:00 +0100

    toaster cmp example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
-<script>
-	import Accordion from '$lib/components/Accordion.svelte';
+<script lang="ts">
+	import { addToast, type ToastData } from '$lib/components/Toaster.svelte';
+
+	const toastData: ToastData[] = [
+		{
+			title: 'Success',
+			description: 'Congratulations! It worked!',
+			color: 'bg-green-500'
+		},
+		{
+			title: 'Warning',
+			description: 'Please check again.',
+			color: 'bg-orange-500'
+		},
+		{
+			title: 'Error',
+			description: 'Something did not work!',
+			color: 'bg-red-500'
+		}
+	];
+
+	function addRandomToast() {
+		addToast({ data: toastData[Math.floor(Math.random() * toastData.length)] });
+	}
 </script>
 
-<Accordion />
+<button
+	class="inline-flex items-center justify-center rounded-md bg-white px-4 py-2 font-medium leading-none
+  text-magnum-700 shadow-lg hover:opacity-75"
+	on:click={addRandomToast}
+>
+	Show toast
+</button>
